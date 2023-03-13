1786700de587150ba89d89ea359aab682578551c
DanPancho <daarcosa@puce.edu.ec>
2023-03-12 22:57:14 -0500

    [main]: Login estilos

--- src/components/Inicio Sesion/index.tsx
+++ src/components/Inicio Sesion/index.tsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import { DivComponent } from './styles';
-
+import React from "react";
+import {
+  ButtonMUI,
+  Component,
+  Componentflex,
+  FormComponent,
+  FullHeightDiv,
+  InputMUI,
+  Title,
+} from "./styles";
+import { FormGroup } from "@mui/material";
+import Image from "next/image";
 const InicioSesionComponent = () => {
   return (
-    <DivComponent>
-        ESTO ES UNA PRUEBA
-    </DivComponent>
-  )
-}
+    <FullHeightDiv>
+      <Component widthComponent="45%" style={{ display: "flex" }}>
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/nutridoc-4ee6b.appspot.com/o/NutriDoc.png?alt=media&token=3020a8bf-650b-4a94-b81b-49c68899137b"
+          alt="nutridoc"
+          width={500}
+          height={400}
+        />
+        <FormComponent>
+          <FormGroup>
+            <div>
+              <Title>Inicio Sesión</Title>
+            </div>
+            <InputMUI
+              id="standard-basic"
+              label="Ingrese su correo electronico"
+              variant="standard"
+              color="success"
+            />
+            <br />
+            <InputMUI
+              id="standard-basic"
+              label="Ingrese su contraseña"
+              variant="standard"
+              color="success"
+            />
+            <br />
+            <ButtonMUI variant="contained" type="submit">
+              Iniciar Sesión
+            </ButtonMUI>
+          </FormGroup>
+        </FormComponent>
+      </Component>
+    </FullHeightDiv>
+  );
+};
 
 export default InicioSesionComponent;
--- src/components/Inicio Sesion/styles.ts
+++ src/components/Inicio Sesion/styles.ts
@@ -1,5 +1,63 @@
-import styled from 'styled-components';
+import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import styled from "styled-components";
 
-export const DivComponent = styled.div`
-    background-color: #000;
-`
+export const FullHeightDiv = styled.div`
+  background-color: #a6f4d5;
+  height: calc(var(--vh, 1vh) * 100);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Componentflex = styled.div`
+  display: flex;
+`;
+export const Component = styled.div<{
+  color?: string;
+  widthComponent?: string;
+}>`
+  background-color: ${(props) =>
+    props.color !== undefined ? props.color : "#fff"};
+  width: ${(props) =>
+    props.widthComponent !== undefined ? props.widthComponent : "100%"};
+  animation: moveY 1.5s ease-in-out;
+
+  @keyframes moveY {
+    0% {
+      transform: translateY(80%);
+    }
+    50%{
+        transform: translateY(-10%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+`;
+
+export const FormComponent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: -60px;
+`;
+
+export const InputMUI = styled(TextField)`
+  width: 300px;
+  margin-bottom: 7%;
+`;
+
+export const ButtonMUI = styled(Button)`
+  margin-top: 10px;
+  width: 100%;
+  align-items: center;
+  background-color: #f4c732;
+  font-size: large;
+  &:hover {
+    background-color: #f4c732;
+  }
+`;
